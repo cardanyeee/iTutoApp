f6e35b38c2536af958458f0ef70502cd11214600
sayrsalad <77481152+sayrsalad@users.noreply.github.com>
2022-01-30 01:54:34 +0800

    theme light

---
+++ app/src/main/java/com/ituto/android/Adapters/ViewPagerAdapter.java
@@ -0,0 +1,76 @@
+package com.ituto.android.Adapters;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.ituto.android.R;
+
+import androidx.annotation.NonNull;
+
+public class ViewPagerAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+
+    public ViewPagerAdapter(Context context) {
+        this.context = context;
+    }
+
+    private int images[] = {
+            R.drawable.undraw_collaborators,
+            R.drawable.undraw_community,
+            R.drawable.undraw_social_interaction
+    };
+
+    private  String titles[] = {
+            "Collaborate",
+            "Find",
+            "Learn"
+    };
+
+    private  String desc[] = {
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    };
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (LinearLayout)object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater)context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.view_pager, container, false);
+
+        ImageView imageView = v.findViewById(R.id.imgViewPager);
+        TextView txtTitle = v.findViewById(R.id.txtTitleViewPager);
+        TextView txtDesc = v.findViewById(R.id.txtDescViewPager);
+
+        imageView.setImageResource(images[position]);
+        txtTitle.setText(titles[position]);
+        txtDesc.setText(desc[position]);
+
+        container.addView(v);
+
+        return v;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((LinearLayout)object);
+    }
+}
--- app/src/main/java/com/ituto/android/MainActivity.java
+++ app/src/main/java/com/ituto/android/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ituto.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.content.Context;
+import android.content.Intent;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                SharedPreferences userPref = getApplicationContext().getSharedPreferences("user",Context.MODE_PRIVATE);
+                boolean isLoggedIn = userPref.getBoolean("isLoggedIn",false);
+
+                if (isLoggedIn){
+//                    startActivity(new Intent(MainActivity.this,HomeActivity.class));
+//                    finish();
+                } else {
+                    isFirstTime();
+                }
+            }
+        }, 1500);
+    }
+
+    private void isFirstTime() {
+        SharedPreferences preferences = getApplication().getSharedPreferences( "onBoard", Context.MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean("isFirstTime", true);
+        if (isFirstTime) {
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isFirstTime", false);
+            editor.apply();
+            startActivity(new Intent( MainActivity.this, OnBoardActivity.class));
+        } else {
+            startActivity(new Intent( MainActivity.this, AuthActivity.class));
+        }
+        finish();
+
     }
 }
